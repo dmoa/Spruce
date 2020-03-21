7865f3047ecafe75bd6f2ba7ef8f99c90d6a793c
dmoa <spammyboy@protonmail.com>
2020-03-21 14:20:37 +0000

    bullet points added

--- simple_paper/PPTX.py
+++ simple_paper/PPTX.py
@@ -1,11 +1,27 @@
 from pptx import Presentation
+from pptx.util import Inches, Pt
 
 def get(prsData):
     prs = Presentation()
-    title_slide_layout = prs.slide_layouts[0]
-    slide = prs.slides.add_slide(title_slide_layout)
 
+    # layout for intro and conclusion slides
+    titleTemplate = prs.slide_layouts[0]
+    # layout for body slides
+    bodyTemplate =  prs.slide_layouts[5]
+
+    slide = prs.slides.add_slide(titleTemplate)
     slide.shapes.title.text = prsData["Title"]
     slide.placeholders[1].text = prsData["Author"]
+
+    for slideData in prsData["Body_Slides"]:
+
+        slide = prs.slides.add_slide(bodyTemplate)
+        slide.shapes.title.text = slideData["Title"]
+
+        left = top = width = height = Inches(1)
+        top = Inches(2)
+        body = slide.shapes.add_textbox(left, top, width, height)
+        # print(slideData["Body"])
+        body.text = slideData["Body"]
 
     return prs
--- simple_paper/UserFile.py
+++ simple_paper/UserFile.py
@@ -10,7 +10,21 @@ def get():
 
     fileData = {
         "Title": lineData[0],
-        "Author": lineData[1]
+        "Author": lineData[1],
+        "Body_Slides": []
     }
+
+    slides = fileData["Body_Slides"]
+    # lineData.pop()
+    for i in range(2, len(lineData)):
+        if lineData[i] == "":
+            slides.append({})
+        else:
+            if slides[-1] == {}:
+                slides[-1].update( {"Title": lineData[i]} )
+                slides[-1].update( {"Body": ""} )
+            else:
+                newBulletPoint = "-\t" + lineData[i][2:] + "\n"
+                slides[-1]["Body"] += newBulletPoint
 
     return fileData
